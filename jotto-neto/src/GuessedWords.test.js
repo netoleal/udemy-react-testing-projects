f297b9657e4ce8f982d20c946d90947ef0abd5a9
Neto Leal <neto.leal@loft.com.br>
2020-04-01 18:01:37 -0300

    Tests and code for non-zero guessed words

--- jotto-neto/src/GuessedWords.test.js
+++ jotto-neto/src/GuessedWords.test.js
@@ -30,15 +30,39 @@ describe('if there are no words guessed', () => {
 
   test('renders without error', () => {
     const component = findByTestAttr(wrapper, 'component-guessed-words');
-
     expect(component.length).toBe(1);
   });
 
   test('renders instructions to guess a word', () => {
     const instructions = findByTestAttr(wrapper, 'guess-instructions');
-
     expect(instructions.text().length).not.toBe(0);
   });
 });
 
-describe('if there are words guessed', () => {});
+describe('if there are words guessed', () => {
+  let wrapper;
+  const guessedWords = [
+    { guessedWord: 'Train', letterMatchCount: 3 },
+    { guessedWord: 'Agile', letterMatchCount: 1 },
+    { guessedWord: 'Party', letterMatchCount: 5 },
+  ];
+
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
+
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words');
+    expect(component.length).toBe(1);
+  });
+
+  test('renders "guessed words" section', () => {
+    const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+    expect(guessedWordsNode.length).toBe(1);
+  });
+
+  test('correct number of guessed words', () => {
+    const guessedWordsNodes = findByTestAttr(wrapper, 'guessed-word');
+    expect(guessedWordsNodes.length).toBe(guessedWords.length);
+  });
+});
